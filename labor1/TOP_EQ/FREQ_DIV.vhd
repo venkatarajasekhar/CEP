262be2a618b4c88aaf77cc81b0ffdc50df5f426f
Michael Schmidt <yoruk@gmx.de>
2014-04-06 17:01:33 +0200

    freqenzteiler mit variable in einem prozess

---
+++ labor1/TOP_EQ/FREQ_DIV.vhd
@@ -0,0 +1,26 @@
+entity FREQ_DIV is
+  generic(CYCLE: natural := 250000); -- 250000 = 25Mhz -> 100Hz
+  port(	CLK_FREQ: in bit;
+	      OUTPUT_FREQ: out bit);
+end FREQ_DIV;
+
+architecture BEHAVIOUR of FREQ_DIV is	
+begin
+
+  p1 : process(CLK_FREQ) 
+	  variable COUNTER: INTEGER := 0;
+  begin
+    if(CLK_FREQ = '1' and CLK_FREQ'event) then	
+      if(COUNTER = CYCLE-1) then 
+        OUTPUT_FREQ <= '1' after 10 ns;
+				COUNTER := 0;
+
+      else
+        OUTPUT_FREQ <= '0' after 10 ns;
+        COUNTER := COUNTER + 1;
+
+      end if;
+    end if;
+  end process p1;
+end BEHAVIOUR;	
+	
