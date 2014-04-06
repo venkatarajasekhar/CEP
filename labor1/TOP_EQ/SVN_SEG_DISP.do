a41727c50a564f1cce0601e7fd3910222ad74759
Michael Schmidt <yoruk@gmx.de>
2014-04-06 23:46:14 +0200

    7segment display umfangreich umgebaut

---
+++ labor1/TOP_EQ/SVN_SEG_DISP.do
@@ -0,0 +1,18 @@
+restart
+
+view wave
+# view signals
+# view processes
+
+delete wave *
+add wave -height 30 sim:/SVN_SEG_DISP/*
+
+# value time, value time (after the actual time value)
+force SVN_SEG_CLK 0 0, 1 150ns -r 300ns
+
+
+# Value of SVN SEG DISP
+force SVN_SEG_VALUE 0xfc30 200ns
+
+
+run 1600 ns
--- labor1/TOP_EQ/SVN_SEG_DISP.vhd
+++ labor1/TOP_EQ/SVN_SEG_DISP.vhd
@@ -1,41 +1,59 @@
 entity SVN_SEG_DISP is
-port( VALUE_DEC: in bit_vector(3 downto 0);   -- 4bit fuer 16 werte
-      DIGIT_DEC: in bit_vector(1 downto 0);   -- 2bit ziffer decoder
-      SEGMENTS: out bit_vector(7 downto 0);   -- 8bit fuer alle segmente
-      ANODE: out bit_vector(3 downto 0));     -- 4bit fuer verschiedene ziffern      
+port( SVN_SEG_CLK: in bit;                          -- clock eingang
+			SVN_SEG_VALUE: in bit_vector(15 downto 0);    -- 16bit = 4x 4bit = 4 ziffern
+      SVN_SEG_ANODE: out bit_vector(3 downto 0);    -- 4bit fuer verschiedene ziffern
+      SVN_SEG_CATHODE: out bit_vector(7 downto 0)); -- 4bit fuer segmente
 end SVN_SEG_DISP;
 
 architecture BEHAVIOUR of SVN_SEG_DISP is
+  signal INT_DIGIT: bit_vector(3 downto 0);
 begin
-  seg: process(VALUE_DEC)
+
+  state: process(SVN_SEG_CLK)
+    variable STATE: INTEGER := 0;
   begin
-    case VALUE_DEC is
-      when "0000" => SEGMENTS <= "11000000";  -- 0
-      when "0001" => SEGMENTS <= "11111001";  -- 1
-      when "0010" => SEGMENTS <= "10100100";  -- 2
-      when "0011" => SEGMENTS <= "10110000";  -- 3
-      when "0100" => SEGMENTS <= "10011001";  -- 4
-      when "0101" => SEGMENTS <= "10010010";  -- 5
-      when "0110" => SEGMENTS <= "10000010";  -- 6
-      when "0111" => SEGMENTS <= "11111000";  -- 7
-      when "1000" => SEGMENTS <= "11111111";  -- 8
-      when "1001" => SEGMENTS <= "10010000";  -- 9
-      when "1010" => SEGMENTS <= "10001000";  -- A
-      when "1011" => SEGMENTS <= "11000011";  -- b
-      when "1100" => SEGMENTS <= "11000110";  -- C
-      when "1101" => SEGMENTS <= "10100001";  -- d
-      when "1110" => SEGMENTS <= "10000110";  -- E
-      when "1111" => SEGMENTS <= "10001110";  -- F
-    end case;
-  end process seg;
-  
-  dig: process(DIGIT_DEC)
+		if(SVN_SEG_CLK'event and SVN_SEG_CLK = '1') then
+      case STATE is
+        when 0 => SVN_SEG_ANODE <= "1110" after 10 ns;
+                    INT_DIGIT <= SVN_SEG_VALUE(3 downto 0) after 10 ns;
+                    STATE := 1;
+        when 1 => SVN_SEG_ANODE <= "1101" after 10 ns;
+                    INT_DIGIT <= SVN_SEG_VALUE(7 downto 4) after 10 ns;
+                    STATE := 2;
+        when 2 => SVN_SEG_ANODE <= "1011" after 10 ns;
+                    INT_DIGIT <= SVN_SEG_VALUE(11 downto 8) after 10 ns;
+                    STATE := 3;
+        when 3 => SVN_SEG_ANODE <= "0111" after 10 ns;
+                    INT_DIGIT <= SVN_SEG_VALUE(15 downto 12) after 10 ns;
+                    STATE := 0; 
+        when others => STATE := 0;   
+        end case;
+      end if;
+  end process state;
+   
+  decode: process(INT_DIGIT)
   begin
-    case DIGIT_DEC is
-      when "00" => ANODE <= "0001";  -- 1
-      when "01" => ANODE <= "0010";  -- 2
-      when "10" => ANODE <= "0100";  -- 3
-      when "11" => ANODE <= "1000";  -- 4
+    case INT_DIGIT is
+      when "0000" => SVN_SEG_CATHODE <= "11000000";  -- 0
+      when "0001" => SVN_SEG_CATHODE <= "11111001";  -- 1
+      when "0010" => SVN_SEG_CATHODE <= "10100100";  -- 2
+      when "0011" => SVN_SEG_CATHODE <= "10110000";  -- 3
+      when "0100" => SVN_SEG_CATHODE <= "10011001";  -- 4
+      when "0101" => SVN_SEG_CATHODE <= "10010010";  -- 5
+      when "0110" => SVN_SEG_CATHODE <= "10000010";  -- 6
+      when "0111" => SVN_SEG_CATHODE <= "11111000";  -- 7
+      when "1000" => SVN_SEG_CATHODE <= "11111111";  -- 8
+      when "1001" => SVN_SEG_CATHODE <= "10010000";  -- 9
+      when "1010" => SVN_SEG_CATHODE <= "10001000";  -- A
+      when "1011" => SVN_SEG_CATHODE <= "11000011";  -- b
+      when "1100" => SVN_SEG_CATHODE <= "11000110";  -- C
+      when "1101" => SVN_SEG_CATHODE <= "10100001";  -- d
+      when "1110" => SVN_SEG_CATHODE <= "10000110";  -- E
+      when "1111" => SVN_SEG_CATHODE <= "10001110";  -- F
     end case;
-  end process dig;
+  end process decode;
+
 end BEHAVIOUR;
+
+  
+  
