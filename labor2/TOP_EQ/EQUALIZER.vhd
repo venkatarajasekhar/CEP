71a0a0b1099a70cf6581bccc02ffc8baf2923f2b
alwinlapp <alwinlapp@haw-hamburg.de>
2014-04-23 15:10:31 +0200

    equalizer auf asynchronen reset und locked umgebaut

--- labor2/TOP_EQ/EQUALIZER.vhd
+++ labor2/TOP_EQ/EQUALIZER.vhd
@@ -8,6 +8,7 @@ entity EQUALIZER is
   generic(COUNT: natural := 128);
   port( CLK_PE: in STD_LOGIC;
         RESET: in STD_LOGIC;
+        LOCKED: in STD_LOGIC;
         Y: in STD_LOGIC_VECTOR(15 downto 0);
         W: out STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
         RDY: out STD_LOGIC := '0');
@@ -17,16 +18,16 @@ architecture BEHAVIOUR OF EQUALIZER is
 begin
 
   Akku: process(CLK_PE)
-	  variable RES: INTEGER := 0;
+	 variable RES: INTEGER := 0;
     variable STEPS: INTEGER := 0;
   begin
-    if(CLK_PE'event and CLK_PE = '1') then
-      if(RESET = '1') then
+    if(RESET = '1' or LOCKED = '1') then
         W <= (others => '0') after 10 ns;
         RDY <= '0' after 10 ns;
         RES := 0;
         STEPS := 0;
-      else
+    elsif(CLK_PE'event and CLK_PE = '1') then
+     
         if(STEPS = 0) then
           RDY <= '0' after 10 ns;
         end if;
