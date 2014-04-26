e50125b51061431517be1d12f37614aa39166e1f
Michael Schmidt <yoruk@gmx.de>
2014-04-26 18:05:54 +0200

    top_eq_tmp und tb in top_eq umbenannt

--- labor2/TOP_EQ/TOP_EQ.vhd
+++ labor2/TOP_EQ/TOP_EQ.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
-entity TOP_EQ_TMP is
+entity TOP_EQ is
   port( NEX: in STD_LOGIC;
         NOE: in STD_LOGIC;
         NWE: in STD_LOGIC;
@@ -14,9 +14,9 @@ entity TOP_EQ_TMP is
         RDY: out STD_LOGIC; --equalizer ausgang
         LOCKED: in STD_LOGIC
         );
-end TOP_EQ_TMP;
+end TOP_EQ;
 
-architecture BEHAVIOUR of TOP_EQ_TMP is
+architecture BEHAVIOUR of TOP_EQ is
 
 signal INT_CLK_SYN: STD_LOGIC;  -- internes clk signal, 350MHz
 signal INT_CLK_PE: STD_LOGIC;  -- internes clk signal, 100MHz
@@ -27,29 +27,34 @@ signal INT_W: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
 signal INT_Y: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
 
 --WRITE_EN Process
-signal NWE1: STD_LOGIC := '1'; 
-signal NWE2: STD_LOGIC := '1';
-signal NWE3: STD_LOGIC := '1';
-signal NWE_SHORT: STD_LOGIC := '1';
+signal INT_NWE1: STD_LOGIC := '1'; 
+signal INT_NWE2: STD_LOGIC := '1';
+signal INT_NWE3: STD_LOGIC := '1';
+signal INT_NWE_SHORT: STD_LOGIC := '1';
 
 --CHIP_EN Process
-signal NEX1: STD_LOGIC := '1'; 
-signal NEX2: STD_LOGIC := '1';
+signal INT_NEX1: STD_LOGIC := '1'; 
+signal INT_NEX2: STD_LOGIC := '1';
 
 --OUTPUT_EN Process
-signal NOE1: STD_LOGIC := '1'; 
-signal NOE2: STD_LOGIC := '1';
+signal INT_NOE1: STD_LOGIC := '1'; 
+signal INT_NOE2: STD_LOGIC := '1';
 
 --ENABLE Process
-signal EN_PULSE: STD_LOGIC := '0';
+signal INT_EN_PULSE: STD_LOGIC := '0';
 
 --PULSE1/2
-signal PE_PULSE: STD_LOGIC := '0';
-signal START: STD_LOGIC := '0';
+signal INT_PE_PULSE: STD_LOGIC := '0';
+signal INT_START: STD_LOGIC := '0';
+
+--INPUT Process
+signal INT_DATA_1: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+signal INT_DATA_2: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
+signal INT_DATA_3: STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
 
 --KOMBINATORISCHE LOGIK
-signal TRISTATE: STD_LOGIC;
-signal EN_0:STD_LOGIC;
+signal INT_TRISTATE: STD_LOGIC;
+signal INT_EN_0:STD_LOGIC;
 
 component EQUALIZER is
   port( CLK_PE: in STD_LOGIC;
@@ -72,7 +77,7 @@ component EQUALIZER is
 
 begin
 
-EQUAL :  EQUALIZER
+EQUAL : EQUALIZER
   port map( CLK_PE => INT_CLK_PE,
             RESET => RESET,
             LOCKED => INT_LOCKED,
@@ -97,80 +102,110 @@ WRITE_EN: process(INT_CLK_SYN)
 begin
   if(RESET = '0' OR INT_LOCKED = '0') then
       
-    NWE1 <= '1' after 1 ns;
-    NWE2 <= '1' after 1 ns;
-    NWE3 <= '1' after 1 ns;
+    INT_NWE1 <= '1' after 1 ns;
+    INT_NWE2 <= '1' after 1 ns;
+    INT_NWE3 <= '1' after 1 ns;
     
   elsif(INT_CLK_SYN'event and INT_CLK_SYN = '1') then      
     
-    NWE1 <= NWE after 1 ns;  -- 1 stufe 
-    NWE2 <= NWE1 after 1 ns;  -- 2 stufe 
-    NWE3 <= NWE2 after 1 ns;  -- 3 stufe
+    INT_NWE1 <= NWE after 1 ns;  -- 1 stufe 
+    INT_NWE2 <= INT_NWE1 after 1 ns;  -- 2 stufe 
+    INT_NWE3 <= INT_NWE2 after 1 ns;  -- 3 stufe
        
   end if;
     -- 4 stufe
-    NWE_SHORT <= NWE3 and not NWE2 after 1 ns;
+    INT_NWE_SHORT <= INT_NWE3 and not INT_NWE2 after 1 ns;
 end process WRITE_EN;
 
 CHIP_EN: process(INT_CLK_SYN)
 begin
   if(RESET = '0' OR INT_LOCKED = '0') then
-    NEX1 <= '1' after 1 ns;
-    NEX2 <= '1' after 1 ns;
+    INT_NEX1 <= '1' after 1 ns;
+    INT_NEX2 <= '1' after 1 ns;
 
   elsif(INT_CLK_SYN'event and INT_CLK_SYN = '1') then
-    NEX1 <= NEX after 1 ns;
-    NEX2 <= NEX1 after 1 ns;
+    INT_NEX1 <= NEX after 1 ns;
+    INT_NEX2 <= INT_NEX1 after 1 ns;
   end if; 
 end process CHIP_EN;
 
 OUTPUT_EN: process(INT_CLK_SYN)
 begin
   if(RESET = '0' OR INT_LOCKED = '0') then
-     NOE1 <= '1' after 1 ns;
-     NOE2 <= '1' after 1 ns;
+     INT_NOE1 <= '1' after 1 ns;
+     INT_NOE2 <= '1' after 1 ns;
 
   elsif(INT_CLK_SYN'event and INT_CLK_SYN = '1') then
-    NOE1 <= NOE after 1 ns;
-    NOE2 <= NOE1 after 1 ns;
+    INT_NOE1 <= NOE after 1 ns;
+    INT_NOE2 <= INT_NOE1 after 1 ns;
   end if; 
 end process OUTPUT_EN;
 
 ENABLE: process(INT_CLK_SYN)
 begin
   if(RESET = '0' OR INT_LOCKED = '0') then
-    EN_PULSE <= '1' after 1 ns;
+    INT_EN_PULSE <= '1' after 1 ns;
     
   elsif(INT_CLK_SYN'event and INT_CLK_SYN = '1') then    
-    EN_PULSE <= EN_0 after 1 ns;
+    INT_EN_PULSE <= INT_EN_0 after 1 ns;
   end if;
 end process ENABLE;
 
-PULSE1: process(EN_PULSE)
+-- nochmal wegen low-aktiv/nicht-low-aktiv logik frau behn fragen
+PULSE1: process(INT_EN_PULSE)
 begin
-  if(START = '1' OR INT_LOCKED = '0') then                --FRAU BEHN FRAGEN
-    PE_PULSE <= '1' after 1 ns;
-  elsif(EN_PULSE'event and EN_PULSE = '1') then
-    PE_PULSE <= '0' after 1 ns;
+  if(INT_START = '1' OR INT_LOCKED = '0') then 
+    INT_PE_PULSE <= '0' after 1 ns;
+  elsif(INT_EN_PULSE'event and INT_EN_PULSE = '1') then
+    INT_PE_PULSE <= '1' after 1 ns;
   end if;
 end process PULSE1;
 
-
-
-
-
---NOCH  NICHT FERTIG, NOCH NICHT KLAR WEGEN START --> KO
 PULSE2: process(INT_CLK_PE)
 begin
   if(RESET = '0' OR INT_LOCKED = '0') then
-    START <= '0' after 1 ns;                             --FRAU BEHN FRAGEN
+    INT_START <= '0' after 1 ns;
   elsif(INT_CLK_PE'event and INT_CLK_PE = '1') then
-    START <= EN_PULSE after 1 ns;
+    INT_START <= INT_EN_PULSE after 1 ns;
   end if;        
 end process PULSE2;
 
+INPUT: process(INT_CLK_SYN)
+begin
+  if(RESET = '0' OR INT_LOCKED = '1') then
+    INT_DATA_2 <= (others => '0') after 1 ns;
+    INT_DATA_3 <= (others => '0') after 1 ns;
+    INT_Y <= (others => '0') after 1 ns;
+  elsif(INT_CLK_SYN'event and INT_CLK_SYN = '1') then
+    INT_DATA_2 <= INT_DATA_1 after 1 ns;                           
+    INT_DATA_3 <= INT_DATA_2 after 1 ns;             
+      
+    if(INT_EN_PULSE = '0') then 
+      INT_Y <= INT_DATA_3 after 1 ns;
+    end if;
+  end if;        
+end process INPUT;
+
 --KOMBINATORISCHE LOGIK
-TRISTATE <= NEX2 OR NOE2 after 1 ns;
-EN_0 <= not NEX2 and NWE_SHORT after 1 ns;
+INT_TRISTATE <= INT_NEX2 OR INT_NOE2 after 1 ns;
+INT_EN_0 <= not INT_NEX2 and INT_NWE_SHORT after 1 ns;
+
+-- von hier nur nch tri-state code
+-- read/write aus sicht der top-eq
+
+-- wenn INT_TRISTATE = 0, dann wird von int_w
+-- nach data geschrieben
+WRITE: process(INT_TRISTATE, INT_W)
+begin
+  if(INT_TRISTATE = '1') then
+    DATA <= (others => 'Z') after 1 ns;
+  else
+    DATA <= INT_W after 1 ns;
+  end if;
+end process WRITE;
+  
+-- lesen geht immer, wird über en im letzten
+-- flip-flop gesteuert  
+READ: INT_DATA_1 <= DATA after 1 ns; 
 end BEHAVIOUR;
 
