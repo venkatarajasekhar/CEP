1f250342fc09a9d11de88129ff7b4f11f897a1e0
Michael Schmidt <yoruk@gmx.de>
2014-05-01 18:10:07 +0200

    equalizer: start eingebaut

--- labor2/TOP_EQ/EQUALIZER.vhd
+++ labor2/TOP_EQ/EQUALIZER.vhd
@@ -5,10 +5,11 @@ use ieee.numeric_std.all;
 --use ieee.std_logic_arith.all;
 
 entity EQUALIZER is
-  generic(COUNT: natural := 128);
+  generic(COUNT: natural);
   port( CLK_PE: in STD_LOGIC;
         RESET: in STD_LOGIC;
         LOCKED: in STD_LOGIC;
+        START: in STD_LOGIC;
         Y: in STD_LOGIC_VECTOR(15 downto 0);
         W: out STD_LOGIC_VECTOR(15 downto 0) := (others => '0');
         RDY: out STD_LOGIC := '0');
@@ -18,16 +19,16 @@ architecture BEHAVIOUR OF EQUALIZER is
 begin
 
   Akku: process(CLK_PE)
-	 variable RES: INTEGER := 0;
+	  variable RES: INTEGER := 0;
     variable STEPS: INTEGER := 0;
   begin
     if(RESET = '0' or LOCKED = '0') then
-        W <= (others => '0') after 1 ns;
-        RDY <= '0' after 1 ns;
-        RES := 0;
-        STEPS := 0;
-    elsif(CLK_PE'event and CLK_PE = '1') then
-     
+      W <= (others => '0') after 1 ns;
+      RDY <= '0' after 1 ns;
+      RES := 0;
+      STEPS := 0;
+    elsif(START = '0') then
+      if(CLK_PE'event and CLK_PE = '1') then
         if(STEPS = 0) then
           RDY <= '0' after 1 ns;
         end if;
@@ -43,6 +44,7 @@ begin
           end if;
 
         STEPS := STEPS + 1;
+      end if;
     end if;
   end process Akku;
 
