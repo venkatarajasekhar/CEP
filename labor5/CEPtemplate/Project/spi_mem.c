fa89144e56148502b97015da5473b53b73f62196
Michael Schmidt <yoruk@gmx.de>
2014-05-26 19:04:03 +0200

    26.05.2014 19:04

--- labor5/CEPtemplate/Project/spi_mem.c
+++ labor5/CEPtemplate/Project/spi_mem.c
@@ -6,7 +6,16 @@
 // specific spi_mem commands
 #define READ_MANUFACTURER_ID 0x9f
 #define READ_ARRAY 0x0b
+#define READ_STATUS_REGISTER 0x05
+#define WRITE_STATUS_REGISTER_BYTE_1 0x01
+#define CHIP_ERASE 0x60
+#define WRITE_ENABLE 0x06
+#define WRITE_DISABLE 0x04
 
+// status register
+#define NEW_SPRL_VALUE 0x80
+#define GLOBAL_PROTECT 0x3c
+#define STATUS_REG_BUSY 0x01
 
 uint8_t spi_mem_ReadByte(unsigned int chip_sel, unsigned int address) {
 	uint8_t ret = 0;
@@ -19,24 +28,70 @@ uint8_t spi_mem_ReadByte(unsigned int chip_sel, unsigned int address) {
 }
 
 
-/*
+///*
 
-1. in speicherzelle gucken ob belegt
-2. wenn ja, block sichern
-3. block loeschen
-4. im gespeicherten block wert aendern
-5. block schreiben
+//1. in speicherzelle gucken ob belegt
+//2. wenn ja, block sichern
+//3. block loeschen
+//4. im gespeicherten block wert aendern
+//5. block schreiben
 
-*/
-void spi_mem_WriteByte(unsigned int chip_sel) {
-	uint8_t ret = 0;
+//*/
+//void spi_mem_WriteByte(unsigned int chip_sel) {
+//	uint8_t ret = 0;
+//	
+//	// unprotect sectors
+//	spi_writeByte(chip_sel, READ_ARRAY);
+//	
+//	ret = spi_readByte(chip_sel);
+//	
+//	return ret;
+//}
+
+void spi_mem_EraseChip(unsigned int chip_sel) {
+	uint8_t status_reg = 0;
+	uint16_t busy = 1;
 	
-	// unprotect sectors
-	spi_writeByte(chip_sel, READ_ARRAY);
+	// write enable
+	spi_writeByte(chip_sel, WRITE_ENABLE);	// send command
 	
-	ret = spi_readByte(chip_sel);
+	// read status register
+	spi_writeByte(chip_sel, READ_STATUS_REGISTER);	// send command
+	status_reg = spi_readByte(chip_sel);						// read byte 1
+	spi_readByte(chip_sel);													// ignore byte 2
 	
-	return ret;
+	// set unprotect
+	status_reg |= GLOBAL_PROTECT;
+	status_reg &= ~NEW_SPRL_VALUE;
+	
+	// write status register
+	spi_writeByte(chip_sel, WRITE_STATUS_REGISTER_BYTE_1);	// send command
+	spi_writeByte(chip_sel, status_reg);										// value
+	
+	// chip erase
+	spi_writeByte(chip_sel, CHIP_ERASE);						// send command
+	
+	// check for ready
+	spi_writeByte(chip_sel, READ_STATUS_REGISTER);	// send command
+	do{
+		busy = spi_readByte(chip_sel);								// read byte 1
+		spi_readByte(chip_sel);												// ignore byte 2
+	}while((busy & STATUS_REG_BUSY) != 0);
+	
+	// read status register
+	spi_writeByte(chip_sel, READ_STATUS_REGISTER);	// send command
+	status_reg = spi_readByte(chip_sel);						// read byte 1
+	spi_readByte(chip_sel);													// ignore byte 2
+	
+	// set protect
+	status_reg &= ~GLOBAL_PROTECT;
+	
+	// write status register
+	spi_writeByte(chip_sel, WRITE_STATUS_REGISTER_BYTE_1);	// send command
+	spi_writeByte(chip_sel, status_reg);										// value
+	
+	// write enable
+	spi_writeByte(chip_sel, WRITE_DISABLE);	// send command
 }
 
 unsigned int spi_mem_ReadManufacturerID(unsigned int chip_sel) {
@@ -44,9 +99,9 @@ unsigned int spi_mem_ReadManufacturerID(unsigned int chip_sel) {
 	
 	spi_writeByte(chip_sel, READ_MANUFACTURER_ID);
 	
-	ret = spi_readByte(chip_sel);
-	ret = ret & (spi_readByte(chip_sel) << 8);
-	ret = ret & (spi_readByte(chip_sel) << 16);
+	ret = spi_readByte(chip_sel);								// manufaturer id
+	ret = ret & (spi_readByte(chip_sel) << 16);	// device id part 1
+	ret = ret & (spi_readByte(chip_sel) << 8);  // device id part 2
 	
 	return ret;
 }
--- labor5/CEPtemplate/Project/spi_mem.h
+++ labor5/CEPtemplate/Project/spi_mem.h
@@ -1,6 +1,7 @@
 #ifndef SPI_MEM_H_
 #define SPI_MEM_H_
 
+void spi_mem_EraseChip(unsigned int chip_sel);
 unsigned int spi_mem_ReadManufacturerID(unsigned int chip_sel);
 
 #endif /* SPI_MEM_ */
