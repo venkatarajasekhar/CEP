446adddb41c1d35aa03d2092d50c7f64fc30ddf1
Michael Schmidt <yoruk@gmx.de>
2014-05-26 17:13:37 +0200

    26.05.2014 17:13

--- labor5/CEPtemplate/Project/spi_mem.c
+++ labor5/CEPtemplate/Project/spi_mem.c
@@ -1,11 +1,45 @@
 #include <stm32f4xx.h>
+#include <stdint.h>
 #include "spi.h"
 #include "spi_mem.h"
 
 // specific spi_mem commands
 #define READ_MANUFACTURER_ID 0x9f
+#define READ_ARRAY 0x0b
 
-unsigned int spi_mem_ReadManufacturerID(unsigned int device_mask, unsigned int chip_sel) {
+
+uint8_t spi_mem_ReadByte(unsigned int chip_sel, unsigned int address) {
+	uint8_t ret = 0;
+	
+	spi_writeByte(chip_sel, READ_ARRAY);
+	
+	ret = spi_readByte(chip_sel);
+	
+	return ret;
+}
+
+
+/*
+
+1. in speicherzelle gucken ob belegt
+2. wenn ja, block sichern
+3. block loeschen
+4. im gespeicherten block wert aendern
+5. block schreiben
+
+*/
+void spi_mem_WriteByte(unsigned int chip_sel) {
+	uint8_t ret = 0;
+	
+	// unprotect sectors
+	spi_writeByte(chip_sel, READ_ARRAY);
+	
+	ret = spi_readByte(chip_sel);
+	
+	return ret;
+}
+
+unsigned int spi_mem_ReadManufacturerID(unsigned int chip_sel) {
 	unsigned int ret = 0;
 	
 	spi_writeByte(chip_sel, READ_MANUFACTURER_ID);
@@ -16,3 +50,5 @@ unsigned int spi_mem_ReadManufacturerID(unsigned int device_mask, unsigned int c
 	
 	return ret;
 }
+
+
--- labor5/CEPtemplate/Project/spi_mem.h
+++ labor5/CEPtemplate/Project/spi_mem.h
@@ -1,6 +1,6 @@
 #ifndef SPI_MEM_H_
 #define SPI_MEM_H_
 
-unsigned int spi_mem_ReadManufacturerID(unsigned int device_mask, unsigned int chip_sel);
+unsigned int spi_mem_ReadManufacturerID(unsigned int chip_sel);
 
 #endif /* SPI_MEM_ */
